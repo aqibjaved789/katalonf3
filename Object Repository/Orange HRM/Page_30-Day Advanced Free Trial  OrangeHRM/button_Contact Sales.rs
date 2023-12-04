bfb640734c159dc74860300bef8d222c44f045bc
aqib.javed <aqib.javed@aqibjaved.fff>
2023-12-05 05:39:48 +1300

    Login test case committed successfully

---
+++ Object Repository/Orange HRM/Page_30-Day Advanced Free Trial  OrangeHRM/button_Contact Sales.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Contact Sales</name>
+   <tag></tag>
+   <elementGuidId>fb406ab4-2d69-4a24-97da-c326a64b14be</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='navbarSupportedContent']/div[2]/ul/li[2]/a/button</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.d-flex.web-menu-btn > ul.nav.navbar-nav.navbar-right.extended-nav > li:nth-of-type(2) > a > button.btn.btn-ohrm.btn-free-demo</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>3b6fda5f-3f58-41f5-98f1-ede81ffaacc5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-ohrm btn-free-demo</value>
+      <webElementGuid>2b055d1d-bb0f-4cec-b286-748a64eb1752</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Contact Sales</value>
+      <webElementGuid>03f20009-fd49-4a5c-8468-bdb3a2dd174c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;navbarSupportedContent&quot;)/div[@class=&quot;d-flex web-menu-btn&quot;]/ul[@class=&quot;nav navbar-nav navbar-right extended-nav&quot;]/li[2]/a[1]/button[@class=&quot;btn btn-ohrm btn-free-demo&quot;]</value>
+      <webElementGuid>d6b34d8d-cebe-4dd4-a0c8-c88e1ca97a5e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='navbarSupportedContent']/div[2]/ul/li[2]/a/button</value>
+      <webElementGuid>429506de-2550-49d6-a246-13a31c919fa5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/ul/li[2]/a/button</value>
+      <webElementGuid>5037475d-f8a4-4914-b372-993864825502</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = 'Contact Sales' or . = 'Contact Sales')]</value>
+      <webElementGuid>533b2b13-452f-49b7-ab9d-244a130b7752</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Orange HRM/Page_Get in Touch with OrangeHRM Sales  OrangeHRM/button_Book a Free Demo.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Book a Free Demo</name>
+   <tag></tag>
+   <elementGuidId>b9d47608-aae8-4970-be1f-aa28c74d89b3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='navbarSupportedContent']/div[2]/ul/li/a/button</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.d-flex.web-menu-btn > ul.nav.navbar-nav.navbar-right.extended-nav > li > a > button.btn.btn-ohrm.btn-contact-sales</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>69f1bb47-c5e9-42eb-9ebb-68f026b5cb3e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-ohrm btn-contact-sales</value>
+      <webElementGuid>0d97d1ca-94fb-4343-87d7-8a44d376a0e3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Book a Free Demo</value>
+      <webElementGuid>3ac67df6-4d0c-4102-8cb4-21e8a1e41b20</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;navbarSupportedContent&quot;)/div[@class=&quot;d-flex web-menu-btn&quot;]/ul[@class=&quot;nav navbar-nav navbar-right extended-nav&quot;]/li[1]/a[1]/button[@class=&quot;btn btn-ohrm btn-contact-sales&quot;]</value>
+      <webElementGuid>6ef8afb5-fff3-43a5-9b38-eb2b52440aca</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='navbarSupportedContent']/div[2]/ul/li/a/button</value>
+      <webElementGuid>2d911850-ecdd-4119-a861-90212a6a6315</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/ul/li/a/button</value>
+      <webElementGuid>59b160ce-edf4-4a52-b07d-2369e04b1153</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = 'Book a Free Demo' or . = 'Book a Free Demo')]</value>
+      <webElementGuid>c36d80d1-30b5-4f7d-905d-78b7d3c9a222</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
